fb951a8075a29eeadb720ac9f6f9354859ff05dd
Ian M <ianmillichamp.2@gmail.com>
2023-02-01 12:16:19 +0000

    added pseudocoding to js file

--- assets/js/index.js
+++ assets/js/index.js
@@ -34,19 +34,22 @@ $(document).ready(function () {
   let todaysWeatherEl = document.getElementById("todaysWeather");
 
   let searchHistory = JSON.parse(localStorage.getItem("search")) || [];
-  let currentDate ="";
+  /**let current Date equal an empty string so we can use the variable later */
+  let currentDate = "";
+  /*use the variable currentSayTime Text to equal a h2 element to give the text basic formatting*/
   let currentDayTimeText = $("<h2>");
+  /**use the variable currentSayTime to equal the class of currentDayTime so the time will be printed in the correct place on the page */
   let currentDayTime = $(".currentDayTime");
 
   /** create a function that will display the current date and time for the user */
   setInterval(
     function () {
-            currentDayTimeText.text(moment().format("dddd, MMMM Do, YYYY H:mm:ss a"));
-            currentDayTime.append(currentDayTimeText);
+      currentDayTimeText.text(moment().format("dddd, MMMM Do, YYYY H:mm:ss a"));
+      currentDayTime.append(currentDayTimeText);
 
     }, 1000);
 
-/**create function to get the weather relating to a particular city name */
+  /**create function to get the weather relating to a particular city name */
   function getWeather(cityName) {
     /*Execute a current weather get request from openweathermap.org using the ajax approach
     first make the variable queryURL equal the url address for the api specifiying that the request is based on the cities name and includes the apiKey generated for the app*/
@@ -58,101 +61,132 @@ $(document).ready(function () {
       url: queryURL,
       method: "GET"
     }).then(function (response) {
-/**console log the response to understand what information you have got back from the api server following the request to it.
- * so basically we have asked for info, we have been sent info based on our specific request and now we can have a look at that answer
- */
-console.log(response);
-        todaysWeatherEl.classList.remove("d-none");
-        // Parse response to display current weather
-        currentDate = moment().format ('D/MM/YYYY'); 
-        let day = moment().format("D");
-        let month = moment().format("MM");
-        let year = moment().format("YYYY");
-        nameEl.innerHTML = response.name + " (" + day + "/" + month + "/" + year + ") ";
+      /**console log the response to understand what information you have got back from the api server following the request to it.
+       * so basically we have asked for info, we have been sent info based on our specific request and now we can have a look at that answer
+       */
+      console.log(response);
+      /* remove the class of d-none from the element todaysWeather to reveal the card coded to contain todays weather for the selected city*/
+      todaysWeatherEl.classList.remove("d-none");
+      /*Parse response to display current weather*/
+      /** redefine current date by using the moments library to seek the required information */
+      currentDate = moment().format('D/MM/YYYY');
+      /**make day = to the current day as per the moments library */
+      let day = moment().format("D");
+      /**make month = to the current month as per the moments library */
+      let month = moment().format("MM");
+      /**make year = to the current year as per the moments library */
+      let year = moment().format("YYYY");
+      /**setting the date in the correct order for uk approach add the correct date with the city name if a response is received for said city inputed by the user */
+      nameEl.innerHTML = response.name + " (" + day + "/" + month + "/" + year + ") ";
+      /**display the weather image received from the api request  by doing the following:*/
+      /**let weather image equal the icon code received in the api request */
       let weatherIMG = response.weather[0].icon;
-        currentIMGEl.setAttribute("src", "https://openweathermap.org/img/wn/" + weatherIMG + "@2x.png");
-        currentIMGEl.setAttribute("alt", response.weather[0].description);
-        currentTempEl.innerHTML = "Temperature: " + fahrenheitToCelsius(response.main.temp) + " 'C";
-        currentPressureEl.innerHTML = "Pressure: " + response.main.pressure + " mbar";
-        currentHumidityEl.innerHTML = "Humidity: " + response.main.humidity + "%";
-        currentWindEl.innerHTML = "WindSpeed: " + response.wind.speed + " MPH";
+      /**add the attribute to currentIMGE1 of the picture located at the url below that has the code equal to the value of weatherImg and is sized to 2x.png */
+      currentIMGEl.setAttribute("src", "https://openweathermap.org/img/wn/" + weatherIMG + "@2x.png");
+      /**add the attribute of the weather description for the requested weather icon as the alt in case the image does not display */
+      currentIMGEl.setAttribute("alt", response.weather[0].description);
 
-        // Get UV Index
-        let cityLat = response.coord.lat;
-        let cityLon = response.coord.lon;
-        let UVQueryURL = `https://api.openweathermap.org/data/2.5/uvi/forecast?lat=${cityLat}&lon=${cityLon}&appid=${myAPIKey}&cnt=1`;
-        $.ajax({
-          url: UVQueryURL,
-          method: "GET"
-        }).then(function (response) {
-            let UVIndex = document.createElement("span");
+      /**to print the temperature for the city add html to the currentTemp element that includes the word temperature, the response from the api request(once it has been converted to celsius using a separate function) and the correct unit describer - 'C */
+      currentTempEl.innerHTML = "Temperature: " + fahrenheitToCelsius(response.main.temp) + " 'C";
+      /**to print the pressure for the city add html to the currentPressure element that includes the word pressure, the response from the api request and the correct unit describer - mbar */
+      currentPressureEl.innerHTML = "Pressure: " + response.main.pressure + " mbar";
+      /**to print the humidity for the city add html to the currentHumidity element that includes the word humidity, the response from the api request and the correct unit describer - % */
+      currentHumidityEl.innerHTML = "Humidity: " + response.main.humidity + "%";
+      /**to print the wind speed for the city add html to the currentWind element that includes the words wind speed, the response from the api request and the correct unit describer - MPH */
+      currentWindEl.innerHTML = "Wind Speed: " + response.wind.speed + " MPH";
 
-            // When UV Index is good, shows green, when ok shows yellow, when bad shows red
-            if (response[0].value < 4) {
-              UVIndex.setAttribute("class", "badge badge-success");
-            }
-            else if (response[0].value < 8) {
-              UVIndex.setAttribute("class", "badge badge-warning");
-            }
-            else {
-              UVIndex.setAttribute("class", "badge badge-danger");
-            }
-            console.log(response[0].value)
-            UVIndex.innerHTML = response[0].value;
-            currentUVEl.innerHTML = "UV Index: ";
-            currentUVEl.append(UVIndex);
-          });
+     /**get the UV INDEX for the selected city and display accordingly 
+      * For this we will need to know the cities latitude and longitude
+     */
+    /**make cityLat equal the latitude of the city as per the response from the api */
+      let cityLat = response.coord.lat;
+      /**make cityLon equal the longitude of the city as per the response from the api */
+      let cityLon = response.coord.lon;
+      /**send a new enquiry to the api based on the cities latitude and longitude and including our api key to authorise the request then use the ajax functiion */
+      let UVQueryURL = `https://api.openweathermap.org/data/2.5/uvi/forecast?lat=${cityLat}&lon=${cityLon}&appid=${myAPIKey}&cnt=1`;
+      $.ajax({
+        url: UVQueryURL,
+        method: "GET"
+      }).then(function (response) {
+        /**parse the reponse from the api to get the correct uv reading for the city */
+        /**make UVIndex equal to craeted html span element  */
+        let UVIndex = document.createElement("span");
 
-        // Get 5 day forecast for this city
-        let cityID = response.id;
-        let fiveDayQueryURL = `https://api.openweathermap.org/data/2.5/forecast?id=${cityID}&appid=${myAPIKey}`;
-        $.ajax({
-          url: fiveDayQueryURL,
-          method: "GET"
-        }).then(function (response)  {
-            fiveDayEl.classList.remove("d-none");
-
-            //  Parse response to display forecast for next 5 days
-            const forecastEls = document.querySelectorAll(".forecast");
-            for (i = 0; i < 6; i++) {
-              forecastEls[i].innerHTML = "";
-              forecastDate = moment().add([i+1],'days').format('D/MM/YYYY');
-              const forecastDateEl = document.createElement("p");
-              $("forecastDate").attr("class", "mt-3 mb-0 forecast-date");
-              forecastDateEl.innerHTML = forecastDate;
-              forecastEls[i].append(forecastDateEl);
-
-              // Icon for forecast weather
-           const forecastWeatherEl = document.createElement("img");
-            forecastWeatherEl.setAttribute("src", "https://openweathermap.org/img/wn/" + response.list[i+1].weather[0].icon + "@2x.png");
-          forecastWeatherEl.setAttribute("alt", response.list[i+1].weather[0].description);
-              forecastEls[i].append(forecastWeatherEl);
-         
-            let forecastTempMinEl = document.createElement("p");
-            let fTemp = kelvinToCelsius(response.list[i+1].main.temp);
-            console.log("data list for min temp",response.list[i+1].main.temp);
-            console.log("Temp = ",fTemp);
-            forecastTempMinEl.innerHTML = "Temperature: " + fTemp +" 'C";
-            forecastEls[i].append(forecastTempMinEl);
-
-            const forecastPressureEl = document.createElement ("p");
-            forecastPressureEl.innerHTML = "Pressure: " + response.list[i+1].main.pressure + "  mbar";
-            forecastEls[i].append(forecastPressureEl);
-
-              const forecastHumidityEl = document.createElement("p");
-              forecastHumidityEl.innerHTML = "Humidity: " + response.list[i+1].main.humidity + "  %";
-              forecastEls[i].append(forecastHumidityEl);
-
-                const forecastWindEl = document.createElement ("p");
-                forecastWindEl.innerHTML = "Wind: " + response.list[i+1].wind.speed + "  MPH";
-                forecastEls[i].append(forecastWindEl);
-
-
-
-
-            }
-          })
+        /* When the UV Index is good, show green, when ok show yellow, when bad show red by doing the following:
+        use an if statement to add the class badge-success (green in bootstrap) to the element if the uv value is less than 4*/
+        if (response[0].value < 4) {
+          UVIndex.setAttribute("class", "badge badge-success");
+        }
+        /**use a else if statement if the response is less than 8 (remember this will be overridden by the if statement if the value is less than
+         * 4, to add the class badge-warning to the span element(bootstrap class = yellow)) */
+        else if (response[0].value < 8) {
+          UVIndex.setAttribute("class", "badge badge-warning");
+        }
+        /**use a else statement if the response is anything else (remember this will be overridden by the else if statement if value less than 8 or the if statement if the value is less than
+         * 4, to add the class badge-danger to the span element(bootstrap class = red))*/
+        else {
+          UVIndex.setAttribute("class", "badge badge-danger");
+        }
+        /**console log the response for value- the uv value */
+        console.log(response[0].value)
+        /**add the value of value to the variable UVIndex */
+        UVIndex.innerHTML = response[0].value;
+        /*add the text uv Index to currentUV element */
+        currentUVEl.innerHTML = "UV Index: ";
+        /**append the variable to the currentUV element so the current uv rating will be displayed */
+        currentUVEl.append(UVIndex);
       });
+
+      /**get the five day forecast for the requested city */
+      let cityID = response.id;
+      let fiveDayQueryURL = `https://api.openweathermap.org/data/2.5/forecast?id=${cityID}&appid=${myAPIKey}`;
+      $.ajax({
+        url: fiveDayQueryURL,
+        method: "GET"
+      }).then(function (response) {
+        fiveDayEl.classList.remove("d-none");
+
+        //  Parse response to display forecast for next 5 days
+        const forecastEls = document.querySelectorAll(".forecast");
+        for (i = 0; i < 6; i++) {
+          forecastEls[i].innerHTML = "";
+          forecastDate = moment().add([i + 1], 'days').format('D/MM/YYYY');
+          const forecastDateEl = document.createElement("p");
+          $("forecastDate").attr("class", "mt-3 mb-0 forecast-date");
+          forecastDateEl.innerHTML = forecastDate;
+          forecastEls[i].append(forecastDateEl);
+
+          // Icon for forecast weather
+          const forecastWeatherEl = document.createElement("img");
+          forecastWeatherEl.setAttribute("src", "https://openweathermap.org/img/wn/" + response.list[i + 1].weather[0].icon + "@2x.png");
+          forecastWeatherEl.setAttribute("alt", response.list[i + 1].weather[0].description);
+          forecastEls[i].append(forecastWeatherEl);
+
+          let forecastTempMinEl = document.createElement("p");
+          let fTemp = kelvinToCelsius(response.list[i + 1].main.temp);
+          console.log("data list for min temp", response.list[i + 1].main.temp);
+          console.log("Temp = ", fTemp);
+          forecastTempMinEl.innerHTML = "Temperature: " + fTemp + " 'C";
+          forecastEls[i].append(forecastTempMinEl);
+
+          const forecastPressureEl = document.createElement("p");
+          forecastPressureEl.innerHTML = "Pressure: " + response.list[i + 1].main.pressure + "  mbar";
+          forecastEls[i].append(forecastPressureEl);
+
+          const forecastHumidityEl = document.createElement("p");
+          forecastHumidityEl.innerHTML = "Humidity: " + response.list[i + 1].main.humidity + "  %";
+          forecastEls[i].append(forecastHumidityEl);
+
+          const forecastWindEl = document.createElement("p");
+          forecastWindEl.innerHTML = "Wind: " + response.list[i + 1].wind.speed + "  MPH";
+          forecastEls[i].append(forecastWindEl);
+
+
+
+
+        }
+      })
+    });
   }
 
   // Get history from local storage if any
@@ -169,15 +203,15 @@ console.log(response);
     localStorage.clear();
     searchHistory = [];
     renderSearchHistory();
-  
+
     window.location.reload();
   })
 
   function fahrenheitToCelsius(f) {
-    return Math.floor((f - 32) * 5/9);
+    return Math.floor((f - 32) * 5 / 9);
   }
   function kelvinToCelsius(k) {
-    return Math.floor (k - 273.15 );
+    return Math.floor(k - 273.15);
   }
 
   function renderSearchHistory() {
